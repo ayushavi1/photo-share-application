4dfa5d7c35fb1ed116d0806473fc5ee59ded5272
ayushavi <ayushavi.123@gmail.com>
2022-06-13 11:10:07 +0530

    Navigation Mobile view done

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = () => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setSideDrawerOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setSideDrawerOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
+      {sideDrawerOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={sideDrawerOpen} onClick={closeDrawerHandler}>
         <nav className='main-navigation__drawer-nav'>
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className='main-navigation__menu-btn'>
-          <span></span>
-          <span></span>
-          <span></span>
+        <button
+          className='main-navigation__menu-btn'
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className='main-navigation__title'>
           <Link to='/'>YourPlaces</Link>
