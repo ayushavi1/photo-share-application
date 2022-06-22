a58d5d4d49f35ba5caeb988a9a42a86c8e11c498
ayushavi <ayushavi.123@gmail.com>
2022-06-22 16:22:29 +0530

    Signup page working

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -9,6 +10,16 @@ const HttpError = require('./models/http-error');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
@@ -27,9 +38,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(
-    'mongodb+srv://ayushavi:ayushdj@cluster0.crrirq3.mongodb.net/places?retryWrites=true&w=majority'
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(5000);
   })
