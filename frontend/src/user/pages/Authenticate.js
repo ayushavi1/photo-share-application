2388e659c89fca831455d63a63538a2951f1e2fd
ayushavi <ayushavi.123@gmail.com>
2022-06-28 05:15:56 +0530

    Added file upload frontend and multer

--- frontend/src/user/pages/Authenticate.js
+++ frontend/src/user/pages/Authenticate.js
@@ -5,6 +5,7 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -36,6 +37,8 @@ const Authenticate = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+
+    console.log(formState.inputs);
     if (!isLoginMode) {
       try {
         const responseData = await sendRequest(
@@ -76,6 +79,7 @@ const Authenticate = () => {
         {
           ...formState.inputs,
           name: undefined,
+          image: undefined,
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -85,6 +89,10 @@ const Authenticate = () => {
           ...formState.inputs,
           name: {
             value: '',
+            isValid: false,
+          },
+          image: {
+            value: null,
             isValid: false,
           },
         },
@@ -112,6 +120,9 @@ const Authenticate = () => {
               errorText='Please enter your name.'
               onInput={inputHandler}
             />
+          )}
+          {!isLoginMode && (
+            <ImageUpload center id='image' onInput={inputHandler} />
           )}
           <Input
             id='email'
